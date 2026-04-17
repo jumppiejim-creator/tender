a05ef0f6bc56c48d25d5e7336f4c23b016dc1043
jumppiejim-creator <jumppiejim@gmail.com>
2026-04-16 22:03:46 -0400

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 var FIREBASE_CONFIG = {
-  apiKey: "AIzaSy...",
+  apiKey: "AIzaSyDgl0b8gB0x20I7t0WS0Ic5UB2bu5_-jzo",
   authDomain: "garden-of-the-stars.firebaseapp.com",
   projectId: "garden-of-the-stars",
-  storageBucket: "garden-of-the-stars.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abc123"
+  storageBucket: "garden-of-the-stars.firebasestorage.app",
+  messagingSenderId: "1097664735238",
+  appId: "1:1097664735238:web:fbe46bf1b44b7e96a6fb87"
 };
